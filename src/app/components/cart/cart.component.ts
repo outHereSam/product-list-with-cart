edd6a33c1e3f4502b0f54ed3bb66f2caa351e4b3
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-21 16:56:04 +0000

    Adds and removes from cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.sass'
+  styleUrl: './cart.component.sass',
 })
 export class CartComponent {
-
+  cartService: CartService = inject(CartService);
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+interface CartItem {
+  id: string;
+  quantity: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cartItems: CartItem[] = [];
+
+  constructor() {}
+
+  getItemQuantity(id: string) {
+    const item = this.cartItems.find((item) => item.id === id);
+
+    if (item == null) {
+      return 0;
+    } else {
+      return item.quantity;
+    }
+  }
+
+  increaseCartQuantity(id: string) {
+    // Find the item in the cartItems array, if it's there increase by 1
+    // if not, add it and make the quantity 1
+    const item = this.cartItems.find((item) => item.id === id);
+
+    if (item == null) {
+      // the item doesn't exists, add it
+      this.cartItems.push({ id, quantity: 1 });
+    } else {
+      // the item exists, increase the quantity
+      item.quantity += 1;
+    }
+  }
+
+  decreaseCartQuantity(id: string) {
+    // Find the item in the cartItems array, if it's there decrease by 1
+    // if not, add it and make the quantity 1
+    // Find the index of the item in the cartItems array
+    const itemIndex = this.cartItems.findIndex((item) => item.id === id);
+
+    if (itemIndex !== -1) {
+      const item = this.cartItems[itemIndex];
+
+      if (item.quantity === 1) {
+        // If the quantity is 1, remove the item from the cart
+        this.cartItems.splice(itemIndex, 1);
+      } else {
+        // Otherwise, decrease the quantity by 1
+        item.quantity -= 1;
+      }
+    }
+  }
+
+  removeFromCart(id: string) {
+    this.cartItems = this.cartItems.filter((item) => item.id !== id);
+  }
+}
