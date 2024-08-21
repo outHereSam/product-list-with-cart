98f957427cdc3ddbdad2132eed0c2d14bbb7447a
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-21 18:22:29 +0000

    Implementssome styles to cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,14 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CartItemComponent } from '../cart-item/cart-item.component';
+import { Dessert } from '../../interfaces/IDessert';
+import { DessertsService } from '../../services/desserts.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, CurrencyPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.sass',
 })
 export class CartComponent {
   cartService: CartService = inject(CartService);
+  dessertsService: DessertsService = inject(DessertsService);
+  protected dessertList: Dessert[] = [];
+
+  constructor() {
+    this.dessertsService.getAllDesserts().then((desserts: Dessert[]) => {
+      this.dessertList = desserts;
+    });
+  }
+
+  getTotalPrice(): number {
+    return this.cartService.cartItems.reduce((total, cartItem) => {
+      const item = this.dessertList.find((i) => i.id === cartItem.id);
+      return total + (item?.price || 0) * cartItem.quantity;
+    }, 0);
+  }
 }
