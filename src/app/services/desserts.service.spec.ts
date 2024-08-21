653e68a99bc701950c008c739de63052810fcf8b
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-21 14:04:39 +0000

    Tries to test service

--- src/app/services/desserts.service.spec.ts
+++ src/app/services/desserts.service.spec.ts
@@ -7,7 +7,7 @@ describe('DessertsService', () => {
     service = new DessertsService();
   });
 
-  it('should return a Dessert List with 9 desserts', (done: DoneFn) => {
+  it('should fetch Dessert List with 9 dessert items', (done: DoneFn) => {
     service.getAllDesserts().then((desserts) => {
       expect(desserts.length).toBe(9);
       done();
