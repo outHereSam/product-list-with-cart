ce0ce6b4984c4cecfb63d5de3b193e3b1821c42f
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-21 11:31:35 +0000

    Begins work on product list

---
+++ src/app/components/dessert-list/dessert-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dessert-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './dessert-list.component.html',
+  styleUrl: './dessert-list.component.sass'
+})
+export class DessertListComponent {
+
+}
